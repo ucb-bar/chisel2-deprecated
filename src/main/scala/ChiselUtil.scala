b96c36482d75e390f7f79cbc0f5647ce8bf9e1f8
Huy Vo <huytbvo@berkeley.edu>
2011-05-23 15:27:39 -0700

    another format for literals: 4~'d5, 6~'hfff000, 2~'b01

--- src/main/scala/ChiselUtil.scala
+++ src/main/scala/ChiselUtil.scala
@@ -6,6 +6,7 @@ class intWrapper(x: Int) {
   def ~(x: Int): Lit = Lit(myVal, 'd', x.toString);
   //def d(x: Int): Lit = Lit(x, myVal);
   //def $(x: String): Lit = Lit(myVal, x(0), x.substring(1, x.length));
+  def ~(x: Symbol): Lit = Lit(myVal, x.name(0), x.name.substring(1, x.name.length))
 }
 
 object ChiselUtil {
