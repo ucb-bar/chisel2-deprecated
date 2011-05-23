b952873c44c6a00f84220798fe851e8c71c1b07d
Huy Vo <huytbvo@berkeley.edu>
2011-05-23 11:29:11 -0700

    decimal literal format 4~5 as well as 4~d5 are supported

--- src/main/scala/ChiselUtil.scala
+++ src/main/scala/ChiselUtil.scala
@@ -3,11 +3,12 @@ package Chisel{
 class intWrapper(x: Int) {
   val myVal = x;
   def ~(x: String): Lit = Lit(myVal, x(0), x.substring(1, x.length));
-  def d(x: Int): Lit = Lit(x, myVal);
-  def $(x: String): Lit = Lit(myVal, x(0), x.substring(1, x.length));
+  def ~(x: Int): Lit = Lit(myVal, 'd', x.toString);
+  //def d(x: Int): Lit = Lit(x, myVal);
+  //def $(x: String): Lit = Lit(myVal, x(0), x.substring(1, x.length));
 }
 
-object intConversion {
+object ChiselUtil {
   implicit def intToIntWrapper(x: Int) = new intWrapper(x);
 }
 
