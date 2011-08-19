f12ab62d142ce2c4585e299467a4fd3d6c7faea1
jmartin <jmartin@eecs.berkeley.edu>
2011-08-19 14:51:33 -0700

    Added ability to use symbols with type bool_t.

--- src/main/scala/bool_t.scala
+++ src/main/scala/bool_t.scala
@@ -8,6 +8,23 @@ object bool_t {
     res.init("", 1);
     res
   }
+  def apply(dir: Symbol): bool_t = {
+    if (dir == null){
+      val res = new bool_t()
+      res.init("", 1)
+      res
+    }
+    else if(dir.equals('input))
+      bool_t(INPUT)
+    else if(dir.equals('output))
+      bool_t(OUTPUT)
+    else {
+      println("INVALID DIRECTION")
+      val res = new bool_t()
+      res.init("", 1)
+      res
+    }
+  }
 }
 
 class bool_t extends int_t {
