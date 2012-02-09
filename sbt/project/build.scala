702b759fb88f9f60acc1b5bdfd7852e9e675270e
Yunsup Lee <yunsup@cs.berkeley.edu>
2012-02-09 14:13:17 -0800

    make a directory for sbt

---
+++ sbt/project/build.scala
@@ -0,0 +1,22 @@
+import sbt._
+import Keys._
+
+object BuildSettings
+{
+  val buildOrganization = "edu.berkeley.cs"
+  val buildVersion = "1.1"
+  val buildScalaVersion = "2.8.1"
+
+  val buildSettings = Defaults.defaultSettings ++ Seq (
+    organization := buildOrganization,
+    version      := buildVersion,
+    scalaVersion := buildScalaVersion
+  )
+}
+
+object ChiselBuild extends Build
+{
+  import BuildSettings._
+
+  lazy val chisel = Project("chisel", file("chisel"), settings = buildSettings)
+}
