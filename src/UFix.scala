f911e792fc236122e10a19508edcd6d1e84f77d4
Huy Vo <huytbvo@berkeley.edu>
2012-02-21 18:53:06 -0800

    ! function makes no sense on UFix types and is removed

--- src/UFix.scala
+++ src/UFix.scala
@@ -57,7 +57,6 @@ class UFix extends Num {
 
   override def unary_-(): UFix = UnaryNodeCell(this, "-"){UFix()};
   override def unary_~(): UFix = UnaryNodeCell(this, "~"){UFix()};
-  def unary_!(): UFix = UnaryNodeCell(this, "!"){UFix()};
   override def andR(): Bool    = ReductionNodeCell(this, "&"){UFix()};
   override def orR():  Bool    = ReductionNodeCell(this, "|"){UFix()};
   override def << (b: UFix): UFix = BinaryNodeCell(this, b, "<<"){UFix()};
