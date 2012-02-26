8a225ca9c1d7ba05e2dea8dc89056fe11547b637
Jonathan Bachrach <jrb@pobox.com>
2012-02-26 10:02:34 -0800

    add assert file

---
+++ src/Assert.scala
@@ -0,0 +1,21 @@
+// author: jonathan bachrach
+
+package Chisel {
+
+import scala.collection.mutable.HashMap;
+import scala.collection.mutable.ListBuffer;
+import Node._;
+import ChiselError._;
+
+object Assert {
+  def apply(cond: Bool, message: String) = 
+    new Assert(cond, message)
+}
+
+
+class Assert(condArg: Bool, val message: String) extends Node {
+  inputs += condArg;
+  def cond = inputs(0);
+}
+
+}
