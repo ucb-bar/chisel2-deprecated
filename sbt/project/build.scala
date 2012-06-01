ade35bef76437d06e58c51c89e7db599eff04147
Huy Vo <huytbvo@berkeley.edu>
2012-05-31 17:47:56 -0700

    simple use case to verify installation

--- sbt/project/build.scala
+++ sbt/project/build.scala
@@ -19,5 +19,5 @@ object ChiselBuild extends Build
   import BuildSettings._
 
   lazy val chisel = Project("chisel", file("chisel"), settings = buildSettings)
-  lazy val test = Project("test", file("test"), settings = buildSettings) dependsOn(chisel)
+  lazy val verify = Project("verify", file("verify"), settings = buildSettings) dependsOn(chisel)
 }
