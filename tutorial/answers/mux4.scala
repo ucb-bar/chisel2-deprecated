271e06d8896abba095093028712a90efd01f716c
Jonathan Bachrach <jrb@pobox.com>
2012-06-07 10:00:09 -0700

    remove tester from answers

--- tutorial/answers/mux4.scala
+++ tutorial/answers/mux4.scala
@@ -26,40 +26,6 @@ class Mux4 extends Component {
   m3.io.in0 := m0.io.out; m3.io.in1 := m1.io.out
 
   io.out := m3.io.out;
-
-  defTests(io) {
-    var allGood = true
-    val vars = new HashMap[Node, Node]()
-    
-    for (s0 <- 0 until 2) {
-      for (s1 <- 0 until 2) {
-        for(i0 <- 0 until 2) {
-          for(i1 <- 0 until 2) {
-            for(i2 <- 0 until 2) {
-              for(i3 <- 0 until 2) {
-                vars.clear()
-                vars(io.sel) = Bits(s1 << 1 | s0)
-                vars(io.in0) = Bits(i0)
-                vars(io.in1) = Bits(i1)
-                vars(io.in2) = Bits(i2)
-                vars(io.in3) = Bits(i3)
-                
-                vars(io.out) = 
-                  if(s1 == 1) { 
-                    if (s0 == 1) Bits(i3) else Bits(i2) 
-                  } else { 
-                    if (s0 == 1) Bits(i1) else Bits(i0) 
-                  }
-                allGood = test(vars) && allGood
-              }
-            }
-          }
-        } 
-      }
-    }
-    allGood
-  }
-
 }
 
 }
