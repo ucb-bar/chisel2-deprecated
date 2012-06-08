5d7cfb5324f037827439d7f97a25c80d2c8d8884
Jonathan Bachrach <jrb@pobox.com>
2012-06-07 23:50:59 -0700

    VendingMachine answer

--- tutorial/src/VendingMachine.scala
+++ tutorial/src/VendingMachine.scala
@@ -12,25 +12,9 @@ class VendingMachine extends Component {
   val sIdle :: s5 :: s10 :: s15 :: sOk :: Nil = 
     Enum(5){ UFix() }
   val state = Reg(resetVal = sIdle)
-  when (state === sIdle) {
-    when (io.nickel) { state := s5 }
-    when (io.dime)   { state := s10 }
-  }
-  when (state === s5) {
-    when (io.nickel) { state := s10 }
-    when (io.dime)   { state := s15 }
-  }
-  when (state === s10) {
-    when (io.nickel) { state := s15 }
-    when (io.dime)   { state := sOk }
-  }
-  when (state === s15) {
-    when (io.nickel) { state := sOk }
-    when (io.dime)   { state := sOk }
-  }
-  when (state === sOk) {
-    state := sIdle
-  }
+
+  // flush out here ...
+
   io.valid := (state === sOk)
 }
 
