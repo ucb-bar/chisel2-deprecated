6b7413fd9e579630278b812b9de95ed898b29ae5
Jonathan Bachrach <jrb@pobox.com>
2012-07-03 10:36:55 -0700

    switch to new getenv CHISEL installation and increase perm to 2G

--- tutorial/sbt/project/build.scala
+++ tutorial/sbt/project/build.scala
@@ -6,6 +6,7 @@ object BuildSettings
   val buildOrganization = "edu.berkeley.cs"
   val buildVersion = "1.1"
   val buildScalaVersion = "2.9.2"
+  // val buildScalaVersion = "2.10.0-M4"
 
   def apply(projectdir: String) = {
     Defaults.defaultSettings ++ Seq (
@@ -21,6 +22,6 @@ object ChiselBuild extends Build
 {
   import BuildSettings._
 
-  lazy val chisel = Project("chisel", file("chisel"), settings = BuildSettings("../.."))
+  lazy val chisel = Project("chisel", file("chisel"), settings = BuildSettings(java.lang.System.getenv("CHISEL")))
   lazy val tutorial = Project("tutorial", file("tutorial"), settings = BuildSettings("..")) dependsOn(chisel)
 }
