cb2666a4e459d74fda8c776f7cd8069f3977b29b
Andrew Waterman <waterman@eecs.berkeley.edu>
2012-10-03 19:01:42 -0700

    generalize Cat support for null

--- src/main/scala/Cat.scala
+++ src/main/scala/Cat.scala
@@ -4,32 +4,14 @@ import Node._
 
 object Cat {
   def apply[T <: Data](mod: T, mods: T*): Bits = {
-    val modsList = mods.toList;
-    if(modsList.length == 1 && modsList(0) == null){
-      mod.setTypeNode(Bits(OUTPUT))
-    } else {
-      val isLit = mods.foldLeft(mod.litOf != null){(a,b) => a && (b.litOf != null)}
-      if (isFolding && isLit) {
-        var res = mod.litOf.value;
-        var tw  = mod.litOf.getWidth();
-        for (n <- mods) {
-          val w = n.litOf.getWidth();
-          res   = (res << w)|n.litOf.value;
-          tw   += w;
-        }
-        Lit(res, tw){ Bits() };
-      } else {
-        // initialize
-        val res = 
-          if(backend.isInstanceOf[VerilogBackend]){
-            val res = new Cat();
-            res.initOf("", sumWidth _, mod.toNode :: mods.toList.map(x => x.toNode))
-          } else {
-            mods.foldLeft(mod.toNode){(a,b) => a ## b.toNode}
-          }
-        res.setTypeNode(Bits(OUTPUT))
-      }
-    }
+    val modsList = (mod :: mods.toList).filter(_ != null).map(_.toNode)
+    val isLit = isFolding && modsList.forall(_.litOf != null)
+    val res = if (!isLit && backend.isInstanceOf[VerilogBackend]) {
+      val res = new Cat();
+      res.initOf("", sumWidth _, modsList)
+    } else
+      modsList.reduceLeft((a, b) => a ## b)
+    res.setTypeNode(Bits(OUTPUT))
   }
 }
 
