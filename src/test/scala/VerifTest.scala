d3d601a9ece87083b4acfbc3cdbc676915ea2dc1
Sebastien Mirolo <smirolo@fortylines.com>
2013-05-09 12:25:39 -0700

    add tests for assert and printf generation

--- src/test/scala/VerifTest.scala
+++ src/test/scala/VerifTest.scala
@@ -52,21 +52,123 @@ class VerifSuite extends AssertionsForJUnit {
     tmpdir.delete()
   }
 
-  @Test def testAssert() {
+  def assertFile( filename: String, content: String ) {
+    val source = scala.io.Source.fromFile(filename, "utf-8")
+    val lines = source.mkString
+    source.close()
+    assert(lines === content)
+  }
 
-    class AssertComp extends Component {
+  @Test def testAssertCpp() {
+
+    class CppAssertComp extends Component {
       val io = new Bundle {
         val x = UFix(INPUT, 8)
         val y = UFix(INPUT, 8)
         val z = UFix(OUTPUT)
       }
-      Assert(Bool(io.x == io.y), "failure")
+      assert(Bool(io.x == io.y), "failure")
+      io.z := Cat(io.x, io.y)
+    }
+
+    chiselMain(Array[String]("--c",
+      "--targetDir", tmpdir.getRoot().toString()),
+      () => new CppAssertComp())
+    assertFile(tmpdir.getRoot() + "/VerifSuite_CppAssertComp_1.cpp",
+"""#include "VerifSuite_CppAssertComp_1.h"
+
+void VerifSuite_CppAssertComp_1_t::init ( bool rand_init ) {
+}
+void VerifSuite_CppAssertComp_1_t::clock_lo ( dat_t<1> reset ) {
+  val_t T1__w0;
+  { T1__w0 = VerifSuite_CppAssertComp_1__io_y.values[0] | VerifSuite_CppAssertComp_1__io_x.values[0] << 8; }
+  { VerifSuite_CppAssertComp_1__io_z.values[0] = T1__w0; }
+  ASSERT(reset.values[0], "failure");
+}
+void VerifSuite_CppAssertComp_1_t::clock_hi ( dat_t<1> reset ) {
+}
+void VerifSuite_CppAssertComp_1_t::print ( FILE* f ) {
+}
+bool VerifSuite_CppAssertComp_1_t::scan ( FILE* f ) {
+  return(!feof(f));
+}
+void VerifSuite_CppAssertComp_1_t::dump(FILE *f, int t) {
+}
+""")
+  }
+
+  @Test def testAssertVerilog() {
+
+    class VerilogAssertComp extends Component {
+      val io = new Bundle {
+        val x = UFix(INPUT, 8)
+        val y = UFix(INPUT, 8)
+        val z = UFix(OUTPUT)
+      }
+      assert(Bool(io.x == io.y), "failure")
       io.z := Cat(io.x, io.y)
     }
 
     chiselMain(Array[String]("--v",
       "--targetDir", tmpdir.getRoot().toString()),
-      () => new AssertComp())
+      () => new VerilogAssertComp())
   }
 
+  @Test def testPrintfCpp() {
+
+    class CppPrintfComp extends Component {
+      val io = new Bundle {
+        val x = UFix(INPUT, 8)
+        val y = UFix(INPUT, 8)
+        val z = UFix(OUTPUT)
+      }
+      printf("display %x %x", io.x, io.y)
+      io.z := Cat(io.x, io.y)
+    }
+    /** XXX Can't run CppBackend back-to-back in the same process
+      because the emulator resource is closed. 
+    chiselMain(Array[String]("--c",
+      "--targetDir", tmpdir.getRoot().toString()),
+      () => new CppPrintfComp())
+    assertFile(tmpdir.getRoot() + "/VerifSuite_CppPrintfComp_1.cpp",
+"""#include "VerifSuite_CppPrintfComp_1.h"
+
+void VerifSuite_CppPrintfComp_1_t::init ( bool rand_init ) {
+}
+void VerifSuite_CppPrintfComp_1_t::clock_lo ( dat_t<1> reset ) {
+  val_t T2__w0;
+  { T2__w0 = VerifSuite_CppPrintfComp_1__io_y.values[0] | VerifSuite_CppPrintfComp_1__io_x.values[0] << 8; }
+  { VerifSuite_CppPrintfComp_1__io_z.values[0] = T2__w0; }
+  T0.values[0] = !reset.values[0];
+}
+void VerifSuite_CppPrintfComp_1_t::clock_hi ( dat_t<1> reset ) {
+}
+void VerifSuite_CppPrintfComp_1_t::print ( FILE* f ) {
+  if (T0.values[0]) dat_fprintf(f, "display %h %h", VerifSuite_CppPrintfComp_1__io_x, VerifSuite_CppPrintfComp_1__io_y);
+}
+bool VerifSuite_CppPrintfComp_1_t::scan ( FILE* f ) {
+  return(!feof(f));
+}
+void VerifSuite_CppPrintfComp_1_t::dump(FILE *f, int t) {
+}
+""")
+      */
+  }
+
+  @Test def testPrintfVerilog() {
+
+    class VerilogPrintfComp extends Component {
+      val io = new Bundle {
+        val x = UFix(INPUT, 8)
+        val y = UFix(INPUT, 8)
+        val z = UFix(OUTPUT)
+      }
+      printf("display %x %x", io.x, io.y)
+      io.z := Cat(io.x, io.y)
+    }
+
+    chiselMain(Array[String]("--v",
+      "--targetDir", tmpdir.getRoot().toString()),
+      () => new VerilogPrintfComp())
+  }
 }
