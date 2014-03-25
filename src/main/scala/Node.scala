15c8cacbee9365c5c17f1b3db8c15eeca113069d
Donggyu Kim <dgkim@s142>
2014-03-25 14:17:17 -0700

    more clean up

--- src/main/scala/Node.scala
+++ src/main/scala/Node.scala
@@ -156,7 +156,7 @@ abstract class Node extends nameable {
   }
 
   def nameIt (path: String, isNamingIo: Boolean) {
-    if( (!named && !isIo) || (isIo && isNamingIo) ) {
+    if( (!isIo && !named) || (isIo && isNamingIo) ) {
       /* If the name was set explicitely through *setName*,
        we don't override it. */
       name = path;
