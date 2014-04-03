097215708cdbb4250bb3c2c58fbe124607efd92f
Jim Lawson <ucbjrl@berkeley.edu>
2014-04-03 11:30:00 -0700

    Revert "Move to sbt 0.13"
    This reverts commit 5fd29183c8e0ffa7dbb08c8fe69d8b4b410b54ca.

--- project/project/plugins.scala
+++ project/project/plugins.scala
@@ -3,6 +3,6 @@ import sbt._
 object Plugins extends Build {
   lazy val plugins = Project("plugins", file("."))
     .dependsOn(
-      uri("git://github.com/bseibel/sbt-simple-junit-xml-reporter-plugin.git#sbt-0.13")
+      uri("git://github.com/bseibel/sbt-simple-junit-xml-reporter-plugin.git")
     )
 }
