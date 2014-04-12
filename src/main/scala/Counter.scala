4e2123446ac77927a9f009264573d3636a560bbb
Donggyu Kim <dgkim@s142>
2014-04-12 02:35:48 -0700

    Different buffer operations depending on whether the target is top

--- src/main/scala/Counter.scala
+++ src/main/scala/Counter.scala
@@ -159,9 +159,17 @@ trait DaisyChain extends Backend {
       ioBuffers(targetPin) = {
         val buf = Reg(UInt())
         if (targetPin.dir == INPUT) {
-          addReg(c, buf, bufName,
-            Map(!DaisyChain.fires(c) -> targetPin)
-          )
+          // Input buffers work differently depending on
+          // whether the target is top or not
+          if (c == Module.topComponent) {
+            addReg(c, buf, bufName,
+              Map(!DaisyChain.fires(c) -> targetPin)
+            )
+          } else {
+            addReg(c, buf, bufName,
+              Map(DaisyChain.fires(c) -> targetPin)
+            )
+          }
           for (consumer <- targetPin.consumers) {
             val idx = consumer.inputs indexOf targetPin
             consumer.inputs(idx) = buf
