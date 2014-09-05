f11e8bf239e6b170f5e6a6cf910d0acb65ffe146
Jim Lawson <ucbjrl@berkeley.edu>
2014-09-05 11:19:14 -0700

    Finished (initial) conversion from Java to Scala.

--- src/main/scala/SCWrapper.scala
+++ src/main/scala/SCWrapper.scala
@@ -1,245 +1,289 @@
 package Chisel
+
+import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable.HashMap
 import scala.io.Source._
-import java.util._
+//import java.util._
 import java.io._
-import static java.lang.String.format
+import java.lang.String.format
 
 object SCWrapper {
-    /*
-   def main(args: Array[String]){
-      //Read in template
-      val fileContents = read_file("src/main/sysc/template.txt")
+  type ReplacementMap = HashMap[String, String]
 
-      //Generate replacements
-      val def = example_component_def2()
-      val replacements = generate_replacements(def)
+  def main(args: Array[String]) {
+    //Read in template
+    val fileContents = read_resource("template.txt")
 
-      //Fill template and write out
-      val filled = fill_template(fileContents, replacements)
-      write_file("generated/generated.cpp", filled)
+    //Generate replacements
+    val ecdef = example_component_def2()
+    val replacements = generate_replacements(ecdef)
 
-      //Debug
-      System.out.println(filled) //DEBUG      
-   }
-    */
+    //Fill template and write out
+    val filled = fill_template(fileContents, replacements)
+    write_file("generated/generated.cpp", filled)
 
-    /*
-   def example_component_def(): ComponentDef {
-      val cdef = new ComponentDef()
-      cdef.ctype = "GCD_t";
-      cdef.name = "GCD";
-      cdef.entries.add(new CEntry("a", true, "dat_t<1>", "GCD__io_a", "GCD__io_r1", "GCD__io_v1"));
-      cdef.entries.add(new CEntry("z", false, "dat_t<1>", "GCD__io_z", "GCD__io_rz", "GCD__io_vz"));
-      return cdef;
-   }
+    //Debug
+    System.out.println(filled) //DEBUG      
+  }
 
-   def example_component_def2(): ComponentDef {
-      val cdef = new ComponentDef();
-      cdef.ctype = "AddFilter_t";
-      cdef.name = "AddFilter";
-      cdef.entries.add(new CEntry("a", true, "dat_t<16>", "AddFilter__io_a", "AddFilter__io_ar", "AddFilter__io_av"));
-      cdef.entries.add(new CEntry("b", false, "dat_t<16>", "AddFilter__io_b", "AddFilter__io_br", "AddFilter__io_bv"));
-      return cdef;
-   }
-    */
+  def example_component_def(): ComponentDef = {
+    val cdef = new ComponentDef("GCD_t", "GCD")
+    cdef.entries += new CEntry("a", true, "dat_t<1>", "GCD__io_a", "GCD__io_r1", "GCD__io_v1")
+    cdef.entries += new CEntry("z", false, "dat_t<1>", "GCD__io_z", "GCD__io_rz", "GCD__io_vz")
+    cdef
+  }
 
-    def genwrapper(c: ComponentDef, filename:  String){
-      //Read in template      
-      val template = read_resource("template.txt")
+  def example_component_def2(): ComponentDef = {
+    val cdef = new ComponentDef("AddFilter_t", "AddFilter")
+    cdef.entries += new CEntry("a", true, "dat_t<16>", "AddFilter__io_a", "AddFilter__io_ar", "AddFilter__io_av")
+    cdef.entries += new CEntry("b", false, "dat_t<16>", "AddFilter__io_b", "AddFilter__io_br", "AddFilter__io_bv")
+    cdef
+  }
 
-      //Generate replacements
-      val replacements = generate_replacements(c)
+  def genwrapper(c: ComponentDef, filename:  String) {
+    //Read in template      
+    val template = read_resource("template.txt")
+  
+    //Generate replacements
+    val replacements = generate_replacements(c)
 
-      //Fill template and write out
-      val filled = fill_template(template, replacements)
-      write_file(filename, filled)
+    //Fill template and write out
+    val filled = fill_template(template, replacements)
+    write_file(filename, filled)
 
-      //Debug
-      System.out.println(filled)
-   }
+    //Debug
+    System.out.println(filled)
+  }
 
-    def genwrapper(c: ComponentDef, filewriter: java.io.FileWriter){
-      //Read in template      
-      val template = read_resource("template.txt")
+  def genwrapper(c: ComponentDef, filewriter: java.io.FileWriter){
+    //Read in template      
+    val template = read_resource("template.txt")
 
-      //Generate replacements
-      val replacements = generate_replacements(c)
+    //Generate replacements
+    val replacements = generate_replacements(c)
 
-      //Fill template and write out
-      val filled = fill_template(template, replacements)
-      write_file(filewriter, filled)
+    //Fill template and write out
+    val filled = fill_template(template, replacements)
+    write_file(filewriter, filled)
 
-      //Debug
-      // System.out.println(filled)
-   }
+    //Debug
+    // System.out.println(filled)
+  }
 
-    def generate_replacements(c: ComponentDef):  Array[Array[String]]{
-      ArrayList<String[]> replacements = new ArrayList<String[]>();
+  def generate_replacements(c: ComponentDef): ReplacementMap = {
+    val replacements = new ReplacementMap()
 
-      //Header file
-      replacements.add(new String[]{"header_file", c.name + ".h"});
+    //Header file
+    replacements += (("header_file", c.name + ".h"))
 
-      //Component name and type
-      replacements.add(new String[]{"name", "SCWrapped" + c.name});
-      replacements.add(new String[]{"component_type", c.ctype});
+    //Component name and type
+    replacements += (("name", "SCWrapped" + c.name))
+    replacements += (("component_type", c.ctype))
 
-      //I/O Fifos
-      /*begin*/{
-         String input_fifos = "";
-         String output_fifos = "";
-         for(CEntry e : c.entries){
-            String decl = format("sc_fifo<%s >* %s;\n  ", e.type, e.name);
-            if(e.is_input)
-               input_fifos += decl;
-            else
-               output_fifos += decl;
-         }
-         replacements.add(new String[]{"input_fifos", input_fifos});
-         replacements.add(new String[]{"output_fifos", output_fifos});
+    //I/O Fifos
+    /*begin*/{
+      var input_fifos = ""
+      var output_fifos = ""
+      for( e <- c.entries) {
+        val decl = "sc_fifo<%s >* %s;\n  ".format(e.ctype, e.name)
+        if(e.is_input) {
+          input_fifos += decl
+        } else {
+          output_fifos += decl
+        }
       }
+      replacements += (("input_fifos", input_fifos))
+      replacements += (("output_fifos", output_fifos))
+    }
 
-      /*Initialize output fifos*/{
-         //Pull out output fifos
-         ArrayList<CEntry> fifos = new ArrayList<CEntry>();
-         for(CEntry e : c.entries)
-            if(!e.is_input) fifos.add(e);
-         //Initialize
-         String init = "";
-         for(int i=0; i<fifos.size(); i++){
-            init += format("%s = new sc_fifo<%s >(1);\n  ", fifos.get(i).name, fifos.get(i).type);
-         }
-         replacements.add(new String[]{"init_output_fifos", init});
+    /*Initialize output fifos*/{
+      //Pull out output fifos
+      val fifos = ArrayBuffer[CEntry]();
+      for(e <- c.entries) {
+        if(!e.is_input) {
+          fifos += e;
+        }
       }
+      //Initialize
+      var init = "";
+      for( i <- 0 until fifos.size) {
+        init += "%s = new sc_fifo<%s >(1);\n  ".format(fifos(i).name, fifos(i).ctype)
+      }
+      replacements += (("init_output_fifos", init))
+    }
 
-      /*Check input queues*/{
-         //Pull out input fifos
-         ArrayList<String> dvar = new ArrayList<String>();
-         ArrayList<String> fvar = new ArrayList<String>();
-         ArrayList<CEntry> fifos = new ArrayList<CEntry>();
-         for(CEntry e : c.entries)
-            if(e.is_input){
-               dvar.add(genvar("dat"));
-               fvar.add(genvar("filled"));
-               fifos.add(e);
+    /*Check input queues*/{
+      //Pull out input fifos
+      val dvar = ArrayBuffer[String]()
+      val fvar = ArrayBuffer[String]()
+      val fifos = ArrayBuffer[CEntry]()
+      for( e <- c.entries) {
+        if(e.is_input) {
+          dvar += genvar("dat")
+          fvar += genvar("filled")
+          fifos += e
+        }
+      }
+      //Initialize
+      var init = ""
+      var fill = ""
+      var check = ""
+      for( i <- 0 until fifos.size) {
+        val ctype = fifos(i).ctype
+        val data = dvar(i)
+        val filled = fvar(i)
+        val in = fifos(i).name
+        val in_data = fifos(i).data
+        val ready = fifos(i).ready
+        val valid = fifos(i).valid
+        init += "%s %s;\n    ".format(ctype, data)
+        init += "int %s = 0;\n    ".format(filled)
+        fill += "if(!%s){%s = %s->nb_read(%s);}\n      "format(filled, filled, in, data)
+        fill += "c->%s = %s;\n      "format(in_data, data)
+        fill += "c->%s = LIT<1>(%s);\n      "format(valid, filled)
+        check += "if(c->%s.values[0]) %s = 0;\n      "format(ready, filled)
+      }
+      replacements += (("input_buffers", init))
+      replacements += (("fill_input", fill))
+      replacements += (("check_input", check))
+    }
+
+    /*Check Output Queues*/{
+      //Pull out output fifos
+      val fifos = ArrayBuffer[CEntry]()
+          for (e <- c.entries) {
+            if(!e.is_input) {
+              fifos += e
             }
-         //Initialize
-         String init = "";
-         String fill = "";
-         String check = "";
-         for(int i=0; i<fifos.size(); i++){
-            String type = fifos.get(i).type;
-            String data = dvar.get(i);
-            String filled = fvar.get(i);
-            String in = fifos.get(i).name;
-            String in_data = fifos.get(i).data;
-            String ready = fifos.get(i).ready;
-            String valid = fifos.get(i).valid;
-            init += format("%s %s;\n    ", type, data);
-            init += format("int %s = 0;\n    ", filled);
-            fill += format("if(!%s){%s = %s->nb_read(%s);}\n      ", filled, filled, in, data);
-            fill += format("c->%s = %s;\n      ", in_data, data);
-            fill += format("c->%s = LIT<1>(%s);\n      ", valid, filled);
-            check += format("if(c->%s.values[0]) %s = 0;\n      ", ready, filled);
-         }
-         replacements.add(new String[]{"input_buffers", init});
-         replacements.add(new String[]{"fill_input", fill});
-         replacements.add(new String[]{"check_input", check});
+          }
+      //Check
+      var check = ""
+      var valid_output = "";
+      for(i <- 0 until fifos.size) {
+        val valid = fifos(i).valid
+        val data = fifos(i).data
+        val ready = fifos(i).ready
+        val out = fifos(i).name
+        check += "c->%s = LIT<1>(%s->num_free() > 0);\n      "format(ready, out)
+        valid_output += "if(c->%s.values[0]) %s->nb_write(c->%s);\n    "format(valid, out, data)
       }
+      replacements += (("check_output", check))
+      replacements += (("valid_output", valid_output))
+    }
 
-      /*Check Output Queues*/{
-         //Pull out output fifos
-         ArrayList<CEntry> fifos = new ArrayList<CEntry>();
-         for(CEntry e : c.entries)
-            if(!e.is_input) fifos.add(e);
-         //Check
-         String check = "";
-         String valid_output = "";
-         for(int i=0; i<fifos.size(); i++){
-            String valid = fifos.get(i).valid;
-            String data = fifos.get(i).data;
-            String ready = fifos.get(i).ready;
-            String out = fifos.get(i).name;
-            check += format("c->%s = LIT<1>(%s->num_free() > 0);\n      ", ready, out);
-            valid_output += format("if(c->%s.values[0]) %s->nb_write(c->%s);\n    ", valid, out, data);
-         }
-         replacements.add(new String[]{"check_output", check});         
-         replacements.add(new String[]{"valid_output", valid_output});         
+    replacements
+  }
+
+  private var unique_counter: Int = 0
+  private def genvar(prefix:  String):  String = {
+    val c = unique_counter
+    unique_counter += 1
+    prefix + c;        
+  }
+
+  def read_file(filename: String): String = {
+    val buffer = new StringBuilder
+    try {
+      val reader = new BufferedReader(new FileReader(filename))
+      var line = ""
+      while({line = reader.readLine(); line != null}) {
+        buffer.append(line)
+        buffer.append("\n")
       }
-      
-      return replacements.toArray(new String[0][]);
-   }
-
-   private static int unique_counter = 0;
-   private static String genvar(String prefix){
-      int c = unique_counter;
-      unique_counter++;
-      return prefix + c;         
-   }
-
-   public static String read_file(String filename){
-      StringBuilder buffer = new StringBuilder();
-      try{
-         BufferedReader reader = new BufferedReader(new FileReader(filename));
-         while(true){
-            String line = reader.readLine();
-            if(line == null) break;
-            buffer.append(line);
-            buffer.append("\n");
-         }
-         reader.close();
-      }catch(IOException e){
-         System.err.println("Error reading file " + filename);
-         System.exit(-1);
+      reader.close()
+    } catch {
+      case e: IOException => {
+        System.err.println("Error reading file " + filename)
+        System.exit(-1)
       }
-      return buffer.toString();
-   }
+    }
+    buffer.toString()
+  }
 
-   public static String read_resource(String resourcename){
-      InputStreamReader resourcestreamReader = new InputStreamReader(SCWrapper.class.getResourceAsStream("/" + resourcename));
-      String line = null;
-      StringBuffer buffer = new StringBuffer("");
-      try{
-         BufferedReader reader = new BufferedReader(resourcestreamReader);
-         while((line = reader.readLine()) != null) {
-           buffer.append(line + "\n");
-         }
-         reader.close();
-      }catch(IOException e){
-         System.err.println("Error reading resource " + resourcename);
-         System.exit(-1);
+  def read_resource(resourcename:  String):  String = {
+    val resourcestreamReader = new InputStreamReader(getClass().getResourceAsStream("/" + resourcename))
+    val buffer = new StringBuilder
+    try {
+      val reader = new BufferedReader(resourcestreamReader)
+      var line = ""
+      while({line = reader.readLine(); line != null}) {
+        buffer.append(line + "\n");
       }
-      return buffer.toString();
-   }
+      reader.close()
+    } catch {
+      case e: IOException => {
+        System.err.println("Error reading resource " + resourcename)
+        System.exit(-1)
+      }
+    }
+    buffer.toString()
+  }
 
-   public static void write_file(String filename, String file){
-      try{
-         BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
-         writer.write(file);
-         writer.close();
-      }catch(IOException e){
-         System.err.println("Error writing file " + filename);
-         System.exit(-1);
+  def write_file(filename: String, file: String) {
+    try {
+      val writer = new BufferedWriter(new FileWriter(filename))
+          writer.write(file)
+          writer.close()
+    } catch {
+      case e: IOException => {
+        System.err.println("Error writing file " + filename)
+        System.exit(-1)
+      }
+    }
+  }
+
+  def write_file(filewriter: java.io.FileWriter, file:  String) {
+    try {
+      val writer = new BufferedWriter(filewriter)
+          writer.write(file)
+          writer.close()
+    } catch {
+      case e:IOException => {
+        System.err.println("Error writing file " + filewriter)
+        System.exit(-1)
       }      
-   }
+    }
+  }
 
-   public static void write_file(java.io.FileWriter filewriter, String file){
-      try{
-         BufferedWriter writer = new BufferedWriter(filewriter);
-         writer.write(file);
-         writer.close();
-      }catch(IOException e){
-         System.err.println("Error writing file " + filewriter);
-         System.exit(-1);
-      }      
-   }
+  def fill_template(template: String, replacements: ReplacementMap): String = {
+    var expansion = template
+    for( (key,value) <- replacements) {
+      val regex = "\\{\\!" + key + "\\!\\}"
+      expansion = regex.r.replaceAllIn(expansion, value)
+    }
+    expansion
+  }
+}
 
-   public static String fill_template(String template, String[][] replacements){
-      for(String[] entry : replacements){
-         String key = entry[0];
-         String value = entry[1];
-         String regex = "\\{\\!" + key + "\\!\\}";
-         template = template.replaceAll(regex, value);
-      }
-      return template;
+class CEntry(a_name: String, input: Boolean, a_type: String, a_data: String, a_ready: String, a_valid: String) {
+   val name = a_name
+   val is_input = input
+   val ctype = a_type
+   val data = a_data
+   val ready = a_ready
+   val valid = a_valid
+
+   override def toString(): String = {
+     name + " " +
+     is_input + " " +
+     ctype + " " +
+     data + " " +
+     ready + " " +
+     valid
    }
+}
+
+class ComponentDef(a_type: String, a_name: String) {
+  val ctype: String = a_type
+  val name: String = a_name
+  val entries = ArrayBuffer[CEntry]()
+
+  override def toString(): String = {
+    var accum: String = ":["
+    for(e <- entries){
+      accum += e + ", "
+    }
+    accum += "]"
+    "Component " + ctype + " " + name + accum
+  }
 }
