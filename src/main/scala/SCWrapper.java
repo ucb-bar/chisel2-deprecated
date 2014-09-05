fa37877002ddfb183537886904bb9b56102072d5
Jim Lawson <ucbjrl@berkeley.edu>
2014-09-05 11:36:45 -0700

    Move Java classes into SCWrapper.java in preparation for Java -> Scala conversion.

--- src/main/scala/SCWrapper.java
+++ src/main/scala/SCWrapper.java
@@ -242,3 +242,49 @@ public class SCWrapper{
       return template;
    }
 }
+
+class CEntry{
+   public String name;
+   public boolean is_input;
+   public String type;
+   public String data;
+   public String ready;
+   public String valid;
+   public CEntry(String a_name, boolean input, String a_type, String a_data, String a_ready, String a_valid){
+      name = a_name;
+      is_input = input;
+      type = a_type;
+      data = a_data;
+      ready = a_ready;
+      valid = a_valid;
+   }
+
+   public String toString(){
+      return
+         name + " " +
+         is_input + " " +
+         type + " " +
+         data + " " +
+         ready + " " +
+         valid;
+   }
+}
+
+class ComponentDef{
+   public String ctype;
+   public String name;   
+   public ArrayList<CEntry> entries;
+   public ComponentDef(){
+      entries = new ArrayList<CEntry>();
+   }
+
+   public String toString(){
+      String accum = ":[";
+      for(CEntry e:entries){
+         accum += e + ", ";
+      }
+      accum += "]";
+
+      return "Component " + ctype + " " + name + entries;
+   }
+}
