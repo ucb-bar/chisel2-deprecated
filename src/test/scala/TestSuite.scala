b7342432b8e39ca06e04c5cb53ef7c5a51c8b053
Jim Lawson <ucbjrl@berkeley.edu>
2014-11-12 12:15:05 -0800

    Add ability to enable --partitionIslands to test.

--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -31,9 +31,20 @@
 import org.scalatest.junit.AssertionsForJUnit
 import org.junit.Before
 import java.io.File
+import scala.collection.mutable.ArrayBuffer
 import scala.reflect.ClassTag
 import scala.collection.JavaConversions
 import Chisel._
+import TestSuite._
+
+object TestSuite {
+
+  // Should we enable some global settings?
+  val partitionIslandsParameterName = "partitionIslands"
+  val defaultEnablePartitionIslands = false
+  val partitionIslandsParameter = System.getProperty(partitionIslandsParameterName)
+  val partitionIslandsEnable = if (partitionIslandsParameter == null) defaultEnablePartitionIslands else true
+}
 
 class TestSuite extends AssertionsForJUnit {
 
@@ -69,10 +80,18 @@ class TestSuite extends AssertionsForJUnit {
     }
   }
 
+
   def launchTester[M <: Module : ClassTag, T <: Tester[M]](b: String, t: M => T) {
     val ctor = implicitly[ClassTag[M]].runtimeClass.getConstructors.head
-    chiselMainTest(Array[String]("--backend", b,
-      "--targetDir", dir.getPath.toString(), "--genHarness", "--compile", "--test"),
+    val conditionalArgs = ArrayBuffer[String]()
+
+    if (partitionIslandsEnable) {
+      conditionalArgs += "--partitionIslands"
+    }
+
+    val testArgs = Array[String]("--backend", b,
+      "--targetDir", dir.getPath.toString(), "--genHarness", "--compile", "--test")
+    chiselMainTest(conditionalArgs.toArray ++ testArgs,
       () => Module(ctor.newInstance(this).asInstanceOf[M])) {t}
   }
   def launchCppTester[M <: Module : ClassTag, T <: Tester[M]](t: M => T) = launchTester("c", t)
