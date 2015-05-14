dcdc9efe6d1a5ea14374bed374b2bdeffe68edda
Jim Lawson <ucbjrl@berkeley.edu>
2015-05-14 11:30:08 -0700

    Add/move preliminary test code.

--- src/main/scala/Complex.scala
+++ src/main/scala/Complex.scala
@@ -132,23 +132,3 @@ class Complex[T<:Data with Num[T]](val real: T, val imag: T) extends Bundle with
     new Complex(-real, -imag)
   }
 }
-
-class ComplexTest extends Module {
-  val io = new Bundle {
-    val in_t = Complex(SInt(width=16),SInt(width=16)).asInput
-    val in_f = Complex(SInt(width=16),SInt(width=16)).asInput
-    val cond = Bool(INPUT)
-    val out  = Complex(SInt(width=16),SInt(width=16)).asOutput
-
-    val b_t = UInt(width=1).asInput
-    val b_f = UInt(width=1).asInput
-    val b_o = UInt(width=1).asOutput
-  }
-
-  val myLit = Complex(SInt(1), SInt(1))
-
-  io.out := Mux(io.cond, io.in_t+io.in_f, io.in_t-io.in_f) + myLit
-
-  io.b_o := Mux(io.cond, io.b_t, Bool(false))
-
-}
