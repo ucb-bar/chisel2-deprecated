0ffb3db4847540a6a488383422f7c9c230ffcc79
Jim Lawson <ucbjrl@berkeley.edu>
2015-05-19 14:14:13 -0700

    Update build_graphs.py for new Jenkins output formats.

--- bin/build_graphs.py
+++ bin/build_graphs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import fnmatch, json, os, re, sys, xml.dom.minidom
+import fnmatch, json, os, re, sys, xml.dom.minidom, time
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -11,62 +11,73 @@ def get_element_text(node):
             result += text.data.strip()
     return result
 
+def load_build_info(path):
+    """Load build.xml as output by Jenkins"""
+    doc = xml.dom.minidom.parse(path)
+    timestampElements = doc.documentElement.getElementsByTagName("timestamp")
+    assert timestampElements.length == 1
+    timestampStr = get_element_text(timestampElements[0])
+    localTimestamp = time.localtime(float(timestampStr)/1000)
+    GMTOffset = time.altzone if localTimestamp.tm_isdst else time.timezone
+    # print "timestamp: %s\n" % (timestampStr)
+    sign = ""
+    if GMTOffset < 0:
+        sign = '-'
+        GMTOffset = -GMTOffset
+    else:
+        sign = '+'
+    hourOffset = GMTOffset/3600
+    minOffset = (GMTOffset - (hourOffset * 3600))/60
+    hhmmOffset = '{}{:02d}{:02d}'.format(sign, hourOffset, minOffset)
+    timestamp = time.strftime("%Y-%m-%dT%H:%M:%S", localTimestamp) + hhmmOffset
+    return timestamp
 
 def load_test_results(path):
-    """Load junitResult.xml as outputed by Jenkins"""
+    """Load junitResult.xml as output by Jenkins"""
     doc = xml.dom.minidom.parse(path)
-    timestamp = get_element_text(
-        doc.documentElement.getElementsByTagName("timestamp")[0])
     nb_tests = len(doc.documentElement.getElementsByTagName("case"))
     nb_failures = len(
         doc.documentElement.getElementsByTagName("errorStackTrace"))
-    return timestamp, nb_tests, nb_failures
+    return nb_tests, nb_failures
 
 def load_checkstyle_violations(path):
-    """Load violations.xml as outputed by Jenkins"""
+    """Load violations.xml as output by Jenkins"""
     nb_violations = 0
     doc = xml.dom.minidom.parse(path)
-    look = re.match('.*/(\d\d\d\d-\d\d-\d\d)_(\d\d)-(\d\d)-(\d\d)/.*', path)
-    if look:
-        timestamp = "%sT%s:%s:%s-0700" % (
-            look.group(1), look.group(2), look.group(3), look.group(4))
-        for node in doc.documentElement.getElementsByTagName("file"):
-            nb_violations += int(node.getAttribute("count"))
-        return timestamp, nb_violations
-    raise ValueError("cannot extract timestamp for %s" % path)
-
+    for node in doc.documentElement.getElementsByTagName("file"):
+        nb_violations += int(node.getAttribute("count"))
+    return nb_violations
 
 def load_coverage(path):
-    """Load coverage.xml as outputed by Jenkins"""
+    """Load coverage.xml as output by Jenkins"""
     doc = xml.dom.minidom.parse(path)
-    look = re.match('.*/(\d\d\d\d-\d\d-\d\d)_(\d\d)-(\d\d)-(\d\d)/.*', path)
-    if look:
-        timestamp = "%sT%s:%s:%s-0700" % (
-            look.group(1), look.group(2), look.group(3), look.group(4))
-        line_coverage = float(doc.documentElement.getAttribute("line-rate"))
-        return timestamp, line_coverage
-    raise ValueError("cannot extract timestamp for %s" % path)
+    line_coverage = float(doc.documentElement.getAttribute("line-rate"))
+    return line_coverage
 
 
 def main(args):
     failures = []
     coverage = []
     violations = []
-    root_dir = os.getcwd()
+    root_dir = os.path.join(os.getcwd(), 'builds')
     for root, dirnames, filenames in os.walk(root_dir):
+        if not ('build.xml' in filenames):
+            continue
+        # General build info
+        timestamp = load_build_info(os.path.join(root, 'build.xml'))
         # unit test results
         for path in fnmatch.filter(filenames, 'junitResult.xml'):
-            timestamp, nb_tests, nb_failures = load_test_results(
+            nb_tests, nb_failures = load_test_results(
                 os.path.join(root, path))
             failures += [ (timestamp, float(nb_failures) / nb_tests) ]
         # checkstyle violations
-        for path in fnmatch.filter(filenames, 'violations.xml'):
-            timestamp, nb_violations = load_checkstyle_violations(
-                os.path.join(root, path))
+        for dir in fnmatch.filter(dirnames, 'violations'):
+            path = os.path.join(root, dir, 'violations.xml')
+            nb_violations = load_checkstyle_violations(path)
             violations += [ (timestamp, nb_violations) ]
         # line coverage
         for path in fnmatch.filter(filenames, 'coverage.xml'):
-            timestamp, line_coverage = load_coverage(
+            line_coverage = load_coverage(
                 os.path.join(root, path))
             coverage += [ (timestamp, line_coverage) ]
 
