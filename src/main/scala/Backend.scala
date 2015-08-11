3a769b099d02daf6be7cbdf352726207baeca227
Stephen Tridgell <stephen@tridgell.net>
2015-08-11 12:17:18 +1000

    Put the first nameAll back in

--- src/main/scala/Backend.scala
+++ src/main/scala/Backend.scala
@@ -833,6 +833,8 @@ abstract class Backend extends FileSystemUtilities{
 
     verifyComponents
 
+    ChiselError.info("giving names")
+    nameAll
     ChiselError.checkpoint()
 
     ChiselError.info("executing custom transforms")
