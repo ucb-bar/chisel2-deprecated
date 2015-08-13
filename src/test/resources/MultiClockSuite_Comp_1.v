ad9ea28a6737bff99ea95e7eaf55fc9e6a984b3f
Jim Lawson <ucbjrl@berkeley.edu>
2015-08-13 14:52:53 -0700

    Update MultiClockSuite_Comp_1.v success code. Don't use T0 for both clock and wire.

--- src/test/resources/MultiClockSuite_Comp_1.v
+++ src/test/resources/MultiClockSuite_Comp_1.v
@@ -1,4 +1,4 @@
-module MultiClockSuite_ClockedSubComp_1(input T0,
+module MultiClockSuite_ClockedSubComp_1(input C0,
     input  io_ready,
     output io_valid
 );
@@ -17,12 +17,12 @@ module MultiClockSuite_ClockedSubComp_1(input T0,
 
   assign io_valid = stored;
 
-  always @(posedge T0) begin
+  always @(posedge C0) begin
     stored <= io_ready;
   end
 endmodule
 
-module MultiClockSuite_Comp_1(input T0,
+module MultiClockSuite_Comp_1(input C0,
     input  io_data0,
     input  io_data1,
     output io_result
@@ -34,7 +34,7 @@ module MultiClockSuite_Comp_1(input T0,
 
   assign T0 = io_data0 & io_data1;
   assign io_result = sub_io_valid;
-  MultiClockSuite_ClockedSubComp_1 sub(.T0(T0),
+  MultiClockSuite_ClockedSubComp_1 sub(.C0(C0),
        .io_ready( T0 ),
        .io_valid( sub_io_valid )
   );
