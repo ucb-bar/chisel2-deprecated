b30065d351d5a71e00b2eb031d9b1eeaca1f4155
Sebastian Bøe <sebastianbooe@gmail.com>
2015-09-19 21:57:13 +0000

    Added test for MuxCase'ing Bundles

--- src/test/scala/StdlibTest.scala
+++ src/test/scala/StdlibTest.scala
@@ -772,6 +772,72 @@ try {
       () => Module(new MuxCaseComp()))
   }
 
+  /** MuxCase can be used with Bundles.
+    */
+  @Test def testMuxCaseWithBundles() {
+    println("\ntestMuxCaseWithBundles ...")
+    class BundleStub extends Bundle {
+      val x = Bool(INPUT)
+
+      override def cloneType: this.type = new BundleStub().asInstanceOf[this.type]
+    }
+
+    class MuxCaseComp extends Module {
+      val io = new Bundle {
+        val sel0 = Bool(INPUT)
+        val sel1 = Bool(INPUT)
+        val in0 = new BundleStub()
+        val in1 = new BundleStub()
+        val out = new BundleStub().flip()
+      }
+      val default = new BundleStub()
+      default.x := Bool(true)
+      io.out := MuxCase(
+        default,
+        Array(
+          io.sel0 -> io.in0,
+          io.sel1 -> io.in1
+        )
+      )
+    }
+
+    class MuxCaseCompTester(m: MuxCaseComp) extends Tester(m) {
+      // set the contents of the in0 and in1 bundles to 0 and 1
+      // respectively and then verify the expected behaviour for all 4
+      // combinations of sel0 and sel0.
+
+      poke(m.io.in0.x, 0)
+      poke(m.io.in1.x, 1)
+
+      poke(m.io.sel0, 0)
+      poke(m.io.sel1, 0)
+      expect(m.io.out.x, 1)
+
+      poke(m.io.sel0, 1)
+      poke(m.io.sel1, 0)
+      expect(m.io.out.x, 0)
+
+      poke(m.io.sel0, 0)
+      poke(m.io.sel1, 1)
+      expect(m.io.out.x, 1)
+
+      poke(m.io.sel0, 1)
+      poke(m.io.sel1, 1)
+      expect(m.io.out.x, 0)
+    }
+
+    chiselMainTest(
+      Array[String](
+        "--backend", "c",
+        "--targetDir", dir.getPath.toString(),
+        "--genHarness",
+        "--compile",
+        "--test"
+      ),
+      () => Module(new MuxCaseComp())
+    ) {m => new MuxCaseCompTester(m)}
+  }
+
   /** Generate a Multiplex
     */
   @Test def testMultiplex() {
