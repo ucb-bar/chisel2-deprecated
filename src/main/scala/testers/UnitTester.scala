5cf0ffad3ca1f152d47bd2a8ccfb9c22ed10b33c
Jim Lawson <ucbjrl@berkeley.edu>
2016-01-06 13:49:11 -0800

    Integrate Chick's UnitTester changes.

--- src/main/scala/testers/UnitTester.scala
+++ src/main/scala/testers/UnitTester.scala
@@ -40,8 +40,8 @@ object Chisel2State {
 }
 
 trait UnitTestRunners {
-  def execute(t: => UnitTester)(implicit args: Array[String]): Boolean = {
-    Chisel2State.args = args
+  def execute(t: => UnitTester)(implicit optionArgs: Array[String]): Boolean = {
+    Chisel2State.args = optionArgs
     try {
       // Construct the combined circuit, containing all the required
       //  poke()'s and expect()'s as arrays of data.
@@ -64,7 +64,8 @@ trait UnitTestRunners {
       }
       true
     } catch {
-      case _: Throwable => 
+      case e: Throwable =>
+        println(e)
         false
     }
   }
@@ -77,9 +78,32 @@ trait UnitTestRunners {
   }
 }
 
-case class Step(input_map: mutable.HashMap[Data,BigInt], output_map: mutable.HashMap[Data,BigInt])
-
+/**
+ * Use a UnitTester to constuct a test harness for a chisel module
+ * this module will be canonically referred to as the device_under_test, often simply as c in
+ * a unit test, and also dut
+ * The UnitTester is used to put series of values (as chisel.Vec's) into the ports of the dut io which are INPUTs
+ * At specified times it check the dut's io OUTPUT ports to see that they match a specific value
+ * The vec's are assembled through the following API
+ * poke, expect and step, pokes
+ *
+ * Example:
+ *
+ * class Adder(width:Int) extends Module {
+ *   val io = new Bundle {
+ *     val in0 : UInt(INPUT, width=width)
+ *     val in1 : UInt(INPUT, width=width)
+ *     val out : UInt(OUTPUT, width=width)
+ *   }
+ * class AdderTester extends UnitTester {
+ *   val device_under_test = Module( new Adder(32) )
+ *   val c = device_under_test
+ *   poke(c.io.in0, 5); poke(c.io.in1, ); poke(
+ *
+ */
 class UnitTester extends Module with UnitTestRunners {
+  case class Step(input_map: mutable.HashMap[Data,BigInt], output_map: mutable.HashMap[Data,BigInt])
+
   val rnd = new scala.util.Random(Driver.testerSeed)
 
   def int(x: Bits):    BigInt = x.litValue()
@@ -87,7 +111,7 @@ class UnitTester extends Module with UnitTestRunners {
   override val io = new Bundle {
     val running       = Bool(INPUT)
     val error         = Bool(OUTPUT)
-    val pc            = UInt(OUTPUT)
+    val pc            = UInt(OUTPUT, 32)
     val done          = Bool(OUTPUT)
   }
   val setDone = Reg(init = Bool(false))
@@ -96,35 +120,30 @@ class UnitTester extends Module with UnitTestRunners {
   var max_width = Width(0)
   val set_input_op :: wait_for_op :: expect_op :: Nil = Enum(UInt(), 3)
 
-  def port_name(dut: Module, port_to_find: Data) : String = {
-    dut.wires.foreach { case (name, port) =>
-        if( port == port_to_find) return name
-    }
-    port_to_find.toString
-  }
+  val ports_referenced = new mutable.HashSet[Data]
 
   // Scala stuff
   val test_actions = new ArrayBuffer[Step]()
   step(1) // gives us a slot to put in our input and outputs from beginning
 
   def poke(io_port: Bits, value: BigInt): Unit = {
-//    println(s"io_port $io_port, len ${test_actions.last.input_map.size} " +
-//            s"ip_port.dir ${io_port.dir}")
-
     require(io_port.dir == INPUT, s"poke error: $io_port not an input")
-//    require(test_actions.last.input_map.contains(io_port) == false,
-//      s"second poke to $io_port without step\nkeys ${test_actions.last.input_map.keys.mkString(",")}")
+    require(test_actions.last.input_map.contains(io_port) == false,
+      s"second poke to $io_port without step\nkeys ${test_actions.last.input_map.keys.mkString(",")}")
 
+    ports_referenced += io_port
     test_actions.last.input_map(io_port) = value
   }
+//  def poke(io_port: Data, bool_value: Boolean) = poke(io_port, if(bool_value) 1 else 0)
 
   def expect(io_port: Bits, value: BigInt): Unit = {
     require(io_port.dir == OUTPUT, s"expect error: $io_port not an output")
     require(!test_actions.last.output_map.contains(io_port), s"second expect to $io_port without step")
 
-//    println(s"io_port $io_port ip_port.dir ${io_port.dir}")
+    ports_referenced += io_port
     test_actions.last.output_map(io_port) = value
   }
+  def expect(io_port: Bits, bool_value: Boolean): Unit = expect(io_port, BigInt(if(bool_value) 1 else 0))
 
   def step(number_of_cycles: Int): Unit = {
     test_actions ++= Array.fill(number_of_cycles) {
@@ -136,6 +155,7 @@ class UnitTester extends Module with UnitTestRunners {
   }
 
   def install[T <: Module](dut: T): Unit = {
+    step(1) // Ensure we have at least one trailing unused action.
     /**
      * connect to the device under test by connecting each of it's io ports to an appropriate register
      */
@@ -146,36 +166,126 @@ class UnitTester extends Module with UnitTestRunners {
       if(element.dir == OUTPUT) Some(element) else None
     }
 
+    val port_to_name = {
+      val port_to_name_accumulator = new mutable.HashMap[Data, String]()
+      def port_name(dut: Module, port_to_find: Data) : String = {
+        dut.wires.foreach { case (name, port) =>
+            if( port == port_to_find) return name
+        }
+        port_to_find.toString
+      }
+
+      println("="*80)
+      println("Device under test: io bundle")
+      println("%10s %10s %s".format("direction", "referenced", "name"))
+      println("-"*80)
+      def parse_bundle(b: Bundle, name: String = ""): Unit = {
+        for ((n, e) <- b.elements) {
+          val new_name = name + (if(name.length > 0 ) "." else "" ) + n
+          port_to_name_accumulator(e) = new_name
+          println("%10s %5s      %s".format(e.toBits.dir, if( ports_referenced.contains(e)) "Y" else " ", new_name))
+
+          e match {
+            case bb: Bundle  => parse_bundle(bb, new_name)
+            case vv: Vec[_]  => parse_vecs(vv, new_name)
+            case ee: Bits => {}
+            case _           => {
+              throw new Exception(s"bad bundle member ${new_name} $e")
+            }
+          }
+        }
+      }
+      def parse_vecs[T<:Data](b: Vec[T], name: String = ""): Unit = {
+        for ((e, i) <- b.zipWithIndex) {
+          val new_name = name + s"($i)"
+          port_to_name_accumulator(e) = new_name
+          println("%10s %5s      %s".format(e.toBits.dir, if( ports_referenced.contains(e)) "Y" else " ", new_name))
+
+          e match {
+            case bb: Bundle  => parse_bundle(bb, new_name)
+            case vv: Vec[_]  => parse_vecs(vv, new_name)
+            case ee: Bits => {}
+            case _           => {
+              throw new Exception(s"bad bundle member ${new_name} $e")
+            }
+          }
+        }
+      }
+
+      dut.io match {
+        case b: Bundle => parse_bundle(b)
+        case x: Data => port_to_name_accumulator(x) = port_name(dut, x)
+      }
+      println("="*80)
+
+      port_to_name_accumulator
+    }
+    /**
+     *  Print a title for the testing state table
+     */
+    val max_col_width = 2 + (if (ports_referenced.isEmpty) {
+      0
+    } else {
+      ports_referenced.map { port =>
+        Array(port_to_name(port).length, port.getWidth / 4).max  // width/4 is how wide value might be in hex
+      }.max
+    })
+    val (string_col_template, number_col_template) = (s"%${max_col_width}s", s"%${max_col_width}x")
+
+    println("="*80)
+    println("UnitTester state table")
+    println(
+      "%6s".format("step") +
+        dut_inputs.map  { dut_input  => string_col_template.format(port_to_name(dut_input))}.mkString +
+        dut_outputs.map { dut_output => string_col_template.format(port_to_name(dut_output))}.mkString
+    )
+    println("-"*80)
     /**
      * prints out a table form of input and expected outputs
      */
-    println(
-      "%6s".format("step") +
-        dut_inputs.map { dut_input => "%8s".format(port_name(dut, dut_input))}.mkString +
-        dut_outputs.map { dut_output => "%8s".format(port_name(dut, dut_output))}.mkString
-    )
     def val_str(hash : mutable.HashMap[Data, BigInt], key: Data) : String = {
-      if( hash.contains(key) ) hash(key).toString else "-"
+      if( hash.contains(key) ) "%x".format(hash(key)) else "-"
     }
     test_actions.zipWithIndex.foreach { case (step, step_number) =>
       print("%6d".format(step_number))
       for(port <- dut_inputs) {
-        print("%8s".format(val_str(step.input_map, port)))
+        print(string_col_template.format(val_str(step.input_map, port)))
       }
       for(port <- dut_outputs) {
-        print("%8s".format(val_str(step.output_map, port)))
+        print(string_col_template.format(val_str(step.output_map, port)))
       }
       println()
     }
+    println("="*80)
+
     io.done  := setDone
     io.error := setError
 
-
-    val pc             = Reg(init=UInt(0, 8))
+    val pc             = Reg(init=UInt(0, log2Up(test_actions.length) + 1))
 
     io.pc := pc
 
-    dut_inputs.foreach { input_port =>
+    def log_referenced_ports: Unit = {
+      val format_statement = new StringBuilder()
+      val port_to_display  = new ArrayBuffer[Data]()
+      format_statement.append("pc: %x")
+      port_to_display.append(pc)
+
+      for( dut_input <- dut_inputs ) {
+        format_statement.append(",  " + port_to_name(dut_input)+": %x")
+        port_to_display.append(dut_input)
+      }
+      for( dut_output <- dut_outputs ) {
+        format_statement.append(",   " + port_to_name(dut_output)+": %x")
+        port_to_display.append(dut_output)
+      }
+      printf(format_statement.toString() + "\n", port_to_display.map{_.toBits()}.toSeq :_* )
+    }
+
+    log_referenced_ports
+
+
+    def create_vectors_for_input(input_port: Data): Unit = {
       var default_value = BigInt(0)
       val input_values = Vec(
         test_actions.map { step =>
@@ -186,7 +296,9 @@ class UnitTester extends Module with UnitTestRunners {
       input_port := input_values(pc)
     }
 
-    dut_outputs.foreach { output_port =>
+    dut_inputs.foreach { port => create_vectors_for_input(port) }
+
+    def create_vectors_and_tests_for_output(output_port: Data): Unit = {
       val output_values = Vec(
         test_actions.map { step =>
           output_port.fromBits(UInt(step.output_map.getOrElse(output_port, BigInt(0))))
@@ -198,24 +310,30 @@ class UnitTester extends Module with UnitTestRunners {
         }
       )
 
-
-//      when(ok_to_test_output_values(pc) && output_port === output_values(pc))) {
       when(ok_to_test_output_values(pc)) {
-        when(output_port.toBits() =/= output_values(pc).toBits()) {
+        when(output_port.toBits() === output_values(pc).toBits()) {
+//          printf("    passed -- " + port_to_name(output_port) + ":  0x%x\n",
+//            output_port.toBits()
+//          )
+        }.otherwise {
+          printf("    failed -- port " + port_to_name(output_port) + ":  0x%x expected 0x%x\n",
+            output_port.toBits(),
+            output_values(pc).toBits()
+          )
           io.error := Bool(true)
           io.done  := Bool(true)
-          printf("Bad value: %x != %x\n", output_port.toBits(), output_values(pc).toBits())
-        } otherwise {
-          printf("Good value: %x == %x\n", output_port.toBits(), output_values(pc).toBits())
         }
       }
     }
 
+    dut_outputs.foreach { port => create_vectors_and_tests_for_output(port) }
+
     when(pc >= UInt(test_actions.length)) {
+      printf(s"Stopping, end of tests, ${test_actions.length} steps\n")
       io.done := Bool(true)
-    } otherwise {
+    }.otherwise {
       pc := pc + UInt(1)
-      printf("bump pc: 0x%x\n", pc)
+//      printf("bump pc: 0x%x\n", pc)
     }
 
   }
