d512f32f6b24ac0ff5701c2c4bf65e203a40d205
Jim Lawson <ucbjrl@berkeley.edu>
2016-01-28 15:15:39 -0800

    Integrate with the Scalatest JUnit infrastructure; add assertVCDFile() and VCDComparator.scala.

--- src/test/scala/RegVcdTest.scala
+++ src/test/scala/RegVcdTest.scala
@@ -28,89 +28,94 @@
  MODIFICATIONS.
 */
 
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.Ignore
+
 import Chisel._
 
-class RegVcdTest(size: Int) extends Module {
-  val io = new Bundle {
-    val din = UInt(INPUT, size)
-    val write = Bool(INPUT)
-    val read = Bool(INPUT)
-    val ready = Bool(OUTPUT)
-    val full = Bool(OUTPUT)
-    val dout = UInt(OUTPUT, size)
-    // for debugging
-    val stateReg = Bool(OUTPUT)
-  }
 
-  val empty :: full :: Nil = Enum(UInt(), 2)
-  val stateReg = Reg(init = empty)
-  val dataReg = Reg(init = Bits(0, size))
-  
-  when (stateReg === empty) {
-    when (io.write) {
-      stateReg := full
-      dataReg := io.din
+class RegVcdSuite extends TestSuite {
+
+  @Test def testRegVcd() {
+    println("\ntestRegVcd...")
+    class RegVcdTest(size: Int) extends Module {
+      val io = new Bundle {
+        val din = UInt(INPUT, size)
+        val write = Bool(INPUT)
+        val read = Bool(INPUT)
+        val ready = Bool(OUTPUT)
+        val full = Bool(OUTPUT)
+        val dout = UInt(OUTPUT, size)
+        // for debugging
+        val stateReg = Bool(OUTPUT)
+      }
+    
+      val empty :: full :: Nil = Enum(UInt(), 2)
+      val stateReg = Reg(init = empty)
+      val dataReg = Reg(init = Bits(0, size))
+      
+      when (stateReg === empty) {
+        when (io.write) {
+          stateReg := full
+          dataReg := io.din
+        }
+      } . elsewhen(stateReg === full) {
+        when (io.read) {
+          stateReg := empty
+        }
+      } .otherwise {
+      }
+    
+      io.ready := (stateReg === empty)
+      io.full := (stateReg === full)
+      io.dout := dataReg
+      
+      io.stateReg := stateReg
     }
-  } . elsewhen(stateReg === full) {
-    when (io.read) {
-      stateReg := empty
+    
+    
+    /**
+     * Test the design.
+     */
+    class RegVcdTestTester(dut: RegVcdTest) extends Tester(dut) {
+    
+      // some defaults for all signals
+      poke(dut.io.write, 0)
+      poke(dut.io.din, 0xab)
+      poke(dut.io.read, 0)
+      step(1)
+      peek(dut.io.ready)
+      
+      // write into the buffer
+      poke(dut.io.din, 0x12)
+      poke(dut.io.write, 1)
+      step(1)
+      peek(dut.io.ready)
+      
+      poke(dut.io.din, 0x34)
+      poke(dut.io.write, 0)
+      step(1)
+      peek(dut.io.ready)
+      
+      // read out
+      poke(dut.io.read, 1)
+      step(1)
+      poke(dut.io.read, 0)
+      step(1)
+      
+      // write next
+      poke(dut.io.din, 0x56)
+      poke(dut.io.write, 1)
+      step(1)
+      peek(dut.io.ready)
     }
-  } .otherwise {
-  }
-
-  io.ready := (stateReg === empty)
-  io.full := (stateReg === full)
-  io.dout := dataReg
-  
-  io.stateReg := stateReg
-}
-
-
-/**
- * Test the design.
- */
-class RegVcdTestTester(dut: RegVcdTest) extends Tester(dut) {
-
-  // some defaults for all signals
-  poke(dut.io.write, 0)
-  poke(dut.io.din, 0xab)
-  poke(dut.io.read, 0)
-  step(1)
-  peek(dut.io.ready)
-  
-  // write into the buffer
-  poke(dut.io.din, 0x12)
-  poke(dut.io.write, 1)
-  step(1)
-  peek(dut.io.ready)
-  
-  poke(dut.io.din, 0x34)
-  poke(dut.io.write, 0)
-  step(1)
-  peek(dut.io.ready)
-  
-  // read out
-  poke(dut.io.read, 1)
-  step(1)
-  poke(dut.io.read, 0)
-  step(1)
-  
-  // write next
-  poke(dut.io.din, 0x56)
-  poke(dut.io.write, 1)
-  step(1)
-  peek(dut.io.ready)
-}
-
-object RegVcdTestTester {
-  def main(args: Array[String]): Unit = {
-
-    chiselMainTest(Array[String](
-      "--genHarness", "--test", "--backend", "c", "--compile", "--vcd"
-      // "--targetDir", dir.getPath.toString()
-      )
-      , () => Module(new RegVcdTest(8))) {
-      f => new RegVcdTestTester(f)
+    
+    val testArgs = chiselEnvironmentArguments() ++ Array("--targetDir", dir.getPath.toString(),
+          "--genHarness", "--test", "--backend", "c", "--compile", "--vcd")
+    chiselMainTest(testArgs, () => Module(new RegVcdTest(8))) {
+          f => new RegVcdTestTester(f)
     }
+    assertVCDFile("RegVcdSuite_RegVcdTest_1.vcd")
   }
 }
