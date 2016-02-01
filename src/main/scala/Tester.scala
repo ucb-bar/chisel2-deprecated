82869cb38ca70815e944303d6ff48081385907a3
Stephen Tridgell <stephen@tridgell.net>
2016-02-01 12:52:26 +1100

    Removed new line in tester to avoid lots of blank lines for multiple step

--- src/main/scala/Tester.scala
+++ src/main/scala/Tester.scala
@@ -492,7 +492,7 @@ class Tester[+T <: Module](c: T, private var isTrace: Boolean = true, _base: Int
     delta += calcDelta
     mwhile(!recvOutputs) { }
     // dumpLogs
-    if (isTrace) println(newTestOutputString)
+    if (isTrace) print(newTestOutputString)
     isStale = false
   }
 
